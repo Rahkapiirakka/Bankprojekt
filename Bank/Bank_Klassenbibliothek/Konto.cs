9a772c9585ff78c0d2ae4092f79844c38e339faf
HelNi <h.killed@gmail.com>
2017-09-21 14:18:54 +0200

    Klassen erstellt: Konto, Transaktion

---
+++ Bank/Bank_Klassenbibliothek/Konto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Konto
+    {
+        /*********************************
+         **                             **
+         **  VARIABLEN & EIGENSCHAFTEN  **
+         **                             **
+         ********************************/
+
+        // Variable & Eigenschaften für die IBAN
+        private string iban;
+
+        public string IBAN
+        {
+            get { return iban; }
+            set { iban = value; }
+        }
+
+
+        // Variable & Eigenschaften für den Kontostand
+        private double kontostand;
+
+        public double Kontostand
+        {
+            get { return kontostand; }
+            set { kontostand = value; }
+        }
+
+
+        // Liste für die Speicherung der Transaktionen
+        List<int> transaktionen = new List<int>();
+
+
+
+
+        /******************************
+        **                           **
+        **  KONSTRUKTOR &FUNKTIONEN  **
+        **                           **
+        ******************************/
+
+        // Konstruktor der Klasse
+        public Konto(string i, double ktostd)
+        {
+            // ...
+        }
+
+
+        // Funktion: Erstellen eines Kontos 
+        public void KontoErstellen()
+        {
+            // ...
+        }
+
+
+        // Funktion
+
+    }
+}
