9bc7d095c6b92edc63faf039436767bf45388a89
HelNi <h.killed@gmail.com>
2017-09-21 13:58:02 +0200

    #1

---
+++ Bank/Bank_Konsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank_Klassenbibliothek;
+
+namespace Bank_Konsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
